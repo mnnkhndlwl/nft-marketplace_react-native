cbfaed90746f6eab5d82b6c9605861bd7d6666c0
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-15 15:25:56 +0530

    focused status bar

---
+++ components/index.js
@@ -0,0 +1,9 @@
+//importing all the components
+//and then exporting them
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+
+export {
+    HomeHeader,NFTCard,FocusedStatusBar
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,13 @@
-import {Text,View,SafeArea,FlatList } from 'react-native';
+import {Text,View,SafeAreaView,FlatList } from 'react-native';
 import { useState } from 'react';
 import { COLORS, FONTS, SIZES, assets } from "../constants";
+import { NFTCard,HomeHeader,FocusedStatusBar } from '../components';
 
 const Home1 = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex:1 }}>
+        <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
   )
 };
 
