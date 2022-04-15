9c772d2d2e52cc0a5754013ee9949ef50b59aea3
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-15 23:23:33 +0530

    commit

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -35,7 +35,7 @@ const NFTCard = ({data}) => {
        width:'100%',
        padding:SIZES.font,
      }}>
-        <NFTTitle /> 
+        <NFTTitle title={data.name} subTitle={data.creator} titleSize={SIZES.large} subTitleSize={SIZES.small} /> 
      </View>
     </View>
   )
--- components/SubInfo.js
+++ components/SubInfo.js
@@ -2,10 +2,11 @@ import { View, Text,Image} from 'react-native'
 import React from 'react'
 import { assets, COLORS, FONTS, SHADOWS, SIZES } from '../constants'
 
-export const NFTTitle = () => {
+export const NFTTitle = ({title,subTitle,titleSize,subTitleSize}) => {
   return (
-    <View>
-      <Text>NFTtitle</Text>
+    <View >
+      <Text style={{fontFamily:FONTS.semiBold,fontSize:titleSize,color:COLORS.primary}}>{title}</Text>
+      <Text style={{fontFamily:FONTS.regular,fontSize:subTitleSize,color:COLORS.primary}}>{subTitle}</Text>
     </View>
   )
 }
