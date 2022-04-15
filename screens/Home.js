fde813ca4e554a45bfcd1254e8794d60c245112b
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-15 16:34:41 +0530

    commit

--- screens/Home.js
+++ screens/Home.js
@@ -10,9 +10,25 @@ const Home1 = () => {
         <View style={{ flex:1 }} >
             {/*  contain our nft list */}
             <View style={{ zIndex: 0 }} >
+            {/* getting data from our dummydata rendering name of each item from dummydata providing a unique and
+             also showing homeheader component */}
             <FlatList data={NFTData}
-                renderItem={({item}) => <Text>{item.name}</Text> }
+                renderItem={({item}) => <Text>{item.name}</Text> } 
+                keyExtractor={(item) => item.id}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={<HomeHeader />}
             />
+            </View>
+            <View style={{
+                position:"absolute",
+                top:0,
+                right:0,
+                left:0,
+                bottom:0,
+                zIndex:-1,
+            }}>
+                <View style={{ height:300,backgroundColor:COLORS.primary }} />
+                <View style={{ flex:1,backgroundColor:COLORS.white }} />
             </View>
         </View>
     </SafeAreaView>
