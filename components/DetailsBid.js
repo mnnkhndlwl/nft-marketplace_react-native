838ccda5fb896b131ef62ebeec5b96cf60b38388
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-20 01:17:35 +0530

    working on search functionality

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,13 +1,59 @@
-//rnfe
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
+import { EthPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
 
-const DetailsBid = () => {
-   
+const DetailsBid = ({ bid }) => {
   return (
-    <Text>hi</Text> 
-  )
-}
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: SIZES.base,
+        paddingHorizontal: SIZES.base,
+      }}
+      key={bid.id}
+    >
+      <Image
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 48, height: 48 }}
+      />
 
-export default DetailsBid
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          paddingHorizontal: SIZES.base,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Bid placed by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.secondary,
+            marginTop: 3,
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
+
+      <EthPrice price={bid.price} />
+    </View>
+  );
+};
+
+export default DetailsBid;
